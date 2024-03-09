76cff0520f9507c1be68defb19a00ba751c24e66
Core447 <jmulmann@protonmail.com>
2024-03-09 14:03:58 +0100

    Add new config options

--- analog_clock/AnalogClockGenerator.py
+++ analog_clock/AnalogClockGenerator.py
@@ -8,13 +8,23 @@ class AnalogClockGenerator:
                  minute_hand_color: tuple = (255, 255, 255, 255),
                  second_hand_color: tuple = (255, 255, 255, 255),
                  origin_color: tuple = (255, 255, 255, 255),
-                 background_color: tuple = (0, 0, 0, 0)):
+                 background_color: tuple = (0, 0, 0, 0),
+                 hour_markings_width: int = 5,
+                 hour_hand_width: int = 7,
+                 minute_hand_width: int = 5,
+                 second_hand_width: int = 3
+                 ):
         
         self.hour_hand_color = hour_hand_color
         self.minute_hand_color = minute_hand_color
         self.second_hand_color = second_hand_color
         self.origin_color = origin_color
         self.background_color = background_color
+
+        self.hour_markings_width = hour_markings_width
+        self.hour_hand_width = hour_hand_width
+        self.minute_hand_width = minute_hand_width
+        self.second_hand_width = second_hand_width
 
     def get_current_clock(self) -> Image.Image:
         now = datetime.now()
@@ -32,7 +42,7 @@ class AnalogClockGenerator:
         center = canvas.size[0] / 2
 
         # Draw hour markings
-        self.draw_hour_markings(draw, center)
+        self.draw_hour_markings(draw, center, width=self.hour_markings_width)
         
         # Draw origin in the center
         radius = 15
@@ -41,17 +51,17 @@ class AnalogClockGenerator:
         # Draw hour hand
         lenght = 150
         angle = hour * (360 / 12) + minute * (360 / 12) / 60
-        self.draw_hand(draw, center, angle, lenght, self.hour_hand_color, 7)
+        self.draw_hand(draw, center, angle, lenght, self.hour_hand_color, self.hour_hand_width)
 
         # Draw minute hand
         lenght = 175
         angle = minute * (360 / 60) + second * (360 / 60) / 60
-        self.draw_hand(draw, center, angle, lenght, self.minute_hand_color, 5)
+        self.draw_hand(draw, center, angle, lenght, self.minute_hand_color, self.minute_hand_width)
 
         # Draw second hand
         lenght = 200
         angle = second * (360 / 60)
-        self.draw_hand(draw, center, angle, lenght, self.second_hand_color, 3)
+        self.draw_hand(draw, center, angle, lenght, self.second_hand_color, self.second_hand_width)
 
 
         return canvas
